367d2bf2b473582c4ff75eb142c8f5a661bb809a
Александр Косолапов <sana.kosolapov@ya.ru>
2025-07-21 14:28:57 +0500

    Swagger работает и в браузере и по API

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using TradingAssistant.Core.Entities;
 using TradingAssistant.Infrastructure;
 using TradingAssistant.Infrastructure.DataBase.MySQL;
@@ -9,16 +10,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Добавляем сервисы Infrastructure (БД)
 builder.Services.AddInfrastructure();
 
-// Настройка OpenAPI
-builder.Services.AddOpenApi();  // Аналог AddSwaggerGen() в .NET 9
+// Настройка OpenAPI/Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "TradingAssistant API", Version = "v1" });
+});
 
 var app = builder.Build();
 
-// Включаем OpenAPI в Development
-if (app.Environment.IsDevelopment())
+// Включаем Swagger UI всегда (можно обернуть в if (app.Environment.IsDevelopment()))
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.MapOpenApi();  // Аналог UseSwaggerUI() в .NET 9
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "TradingAssistant API v1");
+});
 
 app.UseHttpsRedirection();
 
