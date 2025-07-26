f2796804f5c5e847f3e40a28e54041c4a9e66a52
Александр Косолапов <sana.kosolapov@ya.ru>
2025-07-26 18:11:11 +0500

    мелкие правки

--- WebApi/Endpoints/AdminEndpoints.cs
+++ WebApi/Endpoints/AdminEndpoints.cs
@@ -10,32 +10,34 @@ namespace TradingAssistant.WebApi.Endpoints
             var adminGroup = app.MapGroup("/admin").WithTags("Admin endpoints");
 
             adminGroup.MapGet(
-                "/crypto-test/{exchange:alpha}/{type:alpha}", 
+                "/crypto-test/{exchange:alpha}/{type:alpha}",
                 async ([FromServices] ICryptoClient client, [FromRoute] string exchange, [FromRoute] string type) =>
-            {
-                try
                 {
-                    var symbols = type.ToLower() switch
+                    try
                     {
-                        "spot" or "spots" => (dynamic)await client.GetSpotSymbolsAsync(exchange),
-                        "future" or "futures" => (dynamic)await client.GetFuturesSymbolsAsync(exchange),
-                        _ => throw new NotSupportedException($"Unsupported trading type: {type}")
-                    };
-
-                    return symbols != null
-                        ? Results.Ok(new
+                        var symbols = type.ToLower() switch
                         {
-                            Count = symbols.Length,
-                            Symbols = symbols,
-                        })
-                        : Results.NotFound("Активные торговые пары не найдены");
+                            "spot" or "spots" => (dynamic)await client.GetSpotSymbolsAsync(exchange),
+                            "future" or "futures" => (dynamic)await client.GetFuturesSymbolsAsync(exchange),
+                            _ => throw new NotSupportedException($"Unsupported trading type: {type}")
+                        };
+
+                        return symbols != null
+                            ? Results.Ok(new
+                            {
+                                Count = symbols.Length,
+                                Symbols = symbols,
+                            })
+                            : Results.NotFound("Активные торговые пары не найдены");
+                    }
+                    catch (Exception ex)
+                    {
+                        return Results.Problem(
+                            detail: $"Ошибка при получении списка символов: {ex.Message}",
+                            statusCode: StatusCodes.Status500InternalServerError);
+                    }
                 }
-                catch (Exception ex)
-                {
-                    return Results.Problem(
-                        detail: $"Ошибка при получении списка символов: {ex.Message}",
-                        statusCode: StatusCodes.Status500InternalServerError);
-                }
+            );
         }
     }
 }
